7c4c8df3c8001b9a624476ddd7915ef59327a6e7
Ankit Kanojia <119564533+ankitkanojia786@users.noreply.github.com>
2025-04-10 04:51:07 +0530

    Update main.tf

--- infrastructure/modules/react_app/main.tf
+++ infrastructure/modules/react_app/main.tf
@@ -1,13 +1,14 @@
-# modules/react_app/main.tf
 variable "s3_bucket_name" {}
 variable "cloudfront_distribution_id" {}
 variable "app_name" {}
 variable "github_repo" {}
 variable "github_branch" {}
+variable "codestar_connection_arn" {}
 
 # S3 Configuration
 resource "aws_s3_bucket" "react_app" {
   bucket = var.s3_bucket_name
+  force_destroy = true
 }
 
 resource "aws_s3_bucket_website_configuration" "react_app" {
@@ -16,7 +17,12 @@ resource "aws_s3_bucket_website_configuration" "react_app" {
   error_document { key = "index.html" }
 }
 
-# CloudFront Configuration
+resource "aws_s3_bucket_acl" "react_app" {
+  bucket = aws_s3_bucket.react_app.id
+  acl    = "private"
+}
+
+# CloudFront with ALL required blocks
 resource "aws_cloudfront_origin_access_identity" "oai" {
   comment = "OAI for ${var.app_name}"
 }
@@ -25,28 +31,119 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
     domain_name = aws_s3_bucket.react_app.bucket_regional_domain_name
     origin_id   = "S3Origin"
+
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
     }
   }
+
   enabled             = true
+  is_ipv6_enabled     = true
   default_root_object = "index.html"
-  # ... (rest of your CloudFront config) ...
+  comment             = "${var.app_name} distribution"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3Origin"
+    compress         = true
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
 
-# CodePipeline with DEPLOY STAGE
+# CodePipeline with ALL required arguments
+resource "aws_s3_bucket" "artifacts" {
+  bucket = "${var.app_name}-pipeline-artifacts"
+}
+
+resource "aws_iam_role" "codepipeline_role" {
+  name = "${var.app_name}-pipeline-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "codepipeline.amazonaws.com"
+      }
+    }]
+  })
+}
+
 resource "aws_codepipeline" "react_pipeline" {
-  # ... (source and build stages) ...
+  name     = "${var.app_name}-pipeline"
+  role_arn = aws_iam_role.codepipeline_role.arn
+
+  artifact_store {
+    location = aws_s3_bucket.artifacts.bucket
+    type     = "S3"
+  }
+
+  stage {
+    name = "Source"
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeStarSourceConnection"
+      version          = "1"
+      output_artifacts = ["source_output"]
+      configuration    = {
+        ConnectionArn    = var.codestar_connection_arn
+        FullRepositoryId = var.github_repo
+        BranchName       = var.github_branch
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+      version          = "1"
+      configuration    = {
+        ProjectName = "${var.app_name}-build"
+      }
+    }
+  }
 
   stage {
     name = "Deploy"
     action {
       name            = "Deploy"
       category        = "Deploy"
+      owner           = "AWS"
       provider        = "S3"
-      version         = "1"
       input_artifacts = ["build_output"]
-      configuration = {
+      version         = "1"
+      configuration   = {
         BucketName = var.s3_bucket_name
         Extract    = "true"
       }
