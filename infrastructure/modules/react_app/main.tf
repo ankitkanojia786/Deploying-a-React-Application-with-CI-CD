8a3b01c9c00776951aa2735670eebc17dd0c6e40
Ankit Kanojia <119564533+ankitkanojia786@users.noreply.github.com>
2025-04-10 04:41:18 +0530

    Update main.tf

--- infrastructure/modules/react_app/main.tf
+++ infrastructure/modules/react_app/main.tf
@@ -1,61 +1,57 @@
 # modules/react_app/main.tf
-variable "s3_bucket_name" {
-  default = "my-react-app-b38bc729"
-}
+variable "s3_bucket_name" {}
+variable "cloudfront_distribution_id" {}
+variable "app_name" {}
+variable "github_repo" {}
+variable "github_branch" {}
 
-variable "cloudfront_distribution_id" {
-  default = "E2G08P571G5PON"
-}
-
-variable "region" {
-  default = "ap-south-1"
-}
-
-variable "app_name" {
-  default = "my-react-app"
-}
-
-variable "github_repo" {
-  default = "ankitkanojia786/Deploying-a-React-Application-with-CI-CD"
-}
-
-variable "github_branch" {
-  default = "main"
-}
-
-variable "codestar_connection_arn" {}
-
-# S3 Data Source
-data "aws_s3_bucket" "react_app" {
+# S3 Bucket Configuration
+resource "aws_s3_bucket" "react_app" {
   bucket = var.s3_bucket_name
+  force_destroy = true
 }
 
-# CloudFront OAI (Critical Missing Piece)
+resource "aws_s3_bucket_website_configuration" "react_app" {
+  bucket = aws_s3_bucket.react_app.id
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_acl" "react_app" {
+  bucket = aws_s3_bucket.react_app.id
+  acl    = "private"
+}
+
+# CloudFront Configuration
 resource "aws_cloudfront_origin_access_identity" "oai" {
   comment = "OAI for ${var.app_name}"
 }
 
-# S3 Bucket Policy
 resource "aws_s3_bucket_policy" "react_app" {
-  bucket = data.aws_s3_bucket.react_app.id
+  bucket = aws_s3_bucket.react_app.id
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
         Effect    = "Allow",
-        Principal = { AWS = aws_cloudfront_origin_access_identity.oai.iam_arn },
+        Principal = {
+          AWS = aws_cloudfront_origin_access_identity.oai.iam_arn
+        },
         Action    = "s3:GetObject",
-        Resource  = "${data.aws_s3_bucket.react_app.arn}/*"
+        Resource  = "${aws_s3_bucket.react_app.arn}/*"
       }
     ]
   })
 }
 
-# CloudFront Distribution
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
-    domain_name = data.aws_s3_bucket.react_app.bucket_regional_domain_name
-    origin_id   = "S3-${var.app_name}"
+    domain_name = aws_s3_bucket.react_app.bucket_regional_domain_name
+    origin_id   = "S3Origin"
 
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
@@ -69,7 +65,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "S3-${var.app_name}"
+    target_origin_id = "S3Origin"
 
     forwarded_values {
       query_string = false
@@ -95,61 +91,9 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 }
 
-# CodeBuild IAM Role
-resource "aws_iam_role" "codebuild_role" {
-  name = "${var.app_name}-codebuild-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "codebuild.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-# CodeBuild Project
-resource "aws_codebuild_project" "react_app_build" {
-  name          = "${var.app_name}-build"
-  description   = "Build for ${var.app_name}"
-  service_role  = aws_iam_role.codebuild_role.arn
-  build_timeout = 10
-
-  source {
-    type      = "GITHUB"
-    location  = "https://github.com/${var.github_repo}.git"
-    buildspec = "buildspec.yml"
-  }
-
-  environment {
-    compute_type    = "BUILD_GENERAL1_SMALL"
-    image           = "aws/codebuild/standard:6.0"
-    type            = "LINUX_CONTAINER"
-    privileged_mode = true
-
-    environment_variable {
-      name  = "S3_BUCKET"
-      value = var.s3_bucket_name
-    }
-    environment_variable {
-      name  = "CLOUDFRONT_DIST_ID"
-      value = aws_cloudfront_distribution.s3_distribution.id
-    }
-  }
-
-  artifacts {
-    type = "NO_ARTIFACTS"
-  }
-}
-
-# CodePipeline Resources
-resource "aws_s3_bucket" "codepipeline_artifacts" {
-  bucket = "${var.app_name}-pipeline-artifacts"
+# CodePipeline Configuration
+resource "aws_s3_bucket" "artifacts" {
+  bucket = "${var.app_name}-artifacts"
 }
 
 resource "aws_iam_role" "codepipeline_role" {
@@ -174,7 +118,7 @@ resource "aws_codepipeline" "react_pipeline" {
   role_arn = aws_iam_role.codepipeline_role.arn
 
   artifact_store {
-    location = aws_s3_bucket.codepipeline_artifacts.bucket
+    location = aws_s3_bucket.artifacts.bucket
     type     = "S3"
   }
 
@@ -187,9 +131,8 @@ resource "aws_codepipeline" "react_pipeline" {
       provider         = "CodeStarSourceConnection"
       version          = "1"
       output_artifacts = ["source_output"]
-      
       configuration = {
-        ConnectionArn    = var.codestar_connection_arn
+        ConnectionArn    = aws_codestarconnections_connection.github.arn
         FullRepositoryId = var.github_repo
         BranchName       = var.github_branch
       }
@@ -206,9 +149,24 @@ resource "aws_codepipeline" "react_pipeline" {
       input_artifacts  = ["source_output"]
       output_artifacts = ["build_output"]
       version          = "1"
-
       configuration = {
-        ProjectName = aws_codebuild_project.react_app_build.name
+        ProjectName = aws_codebuild_project.react_app.name
+      }
+    }
+  }
+
+  stage {
+    name = "Deploy"
+    action {
+      name            = "Deploy"
+      category        = "Deploy"
+      owner           = "AWS"
+      provider        = "S3"
+      input_artifacts = ["build_output"]
+      version         = "1"
+      configuration = {
+        BucketName = var.s3_bucket_name
+        Extract    = "true"
       }
     }
   }
@@ -216,13 +174,5 @@ resource "aws_codepipeline" "react_pipeline" {
 
 # Outputs
 output "cloudfront_url" {
-  value = "https://${aws_cloudfront_distribution.s3_distribution.domain_name}"
-}
-
-output "s3_bucket_name" {
-  value = data.aws_s3_bucket.react_app.bucket
-}
-
-output "codepipeline_name" {
-  value = aws_codepipeline.react_pipeline.name
+  value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
