208603dbcbc86e3e84006f9480071b3f891a4e9a
Ankit Kanojia <119564533+ankitkanojia786@users.noreply.github.com>
2025-04-10 05:19:22 +0530

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,24 +1,17 @@
 terraform {
   required_version = ">= 1.5.0"
-  backend "s3" {
-    bucket = "my-react-app-tfstate"
-    key    = "terraform.tfstate"
-    region = "ap-south-1"
-  }
+  # REMOVE backend block - keep it only in backend.tf
 }
 
-provider "aws" {
-  region = "ap-south-1"
-}
+# REMOVE provider block - keep it only in provider.tf
 
 module "react_app" {
   source = "./modules/react_app"
 
-  # Required variables with your specific values
-  app_name                  = "my-react-app"
-  github_repo               = "ankitkanojia786/Deploying-a-React-Application-with-CI-CD"
-  github_branch             = "main"
-  s3_bucket_name            = "my-react-app-b38bc729"
+  app_name                = "my-react-app"
+  github_repo             = "ankitkanojia786/Deploying-a-React-Application-with-CI-CD" 
+  github_branch           = "main"
+  s3_bucket_name          = "my-react-app-b38bc729"
   cloudfront_distribution_id = "E2G08P571G5PON"
-  codestar_connection_arn   = "arn:aws:codestar-connections:ap-south-1:860265990835:connection/b9b175eb-c417-44dc-8e1b-332d71300d5a"
+  codestar_connection_arn = "arn:aws:codestar-connections:ap-south-1:860265990835:connection/b9b175eb-c417-44dc-8e1b-332d71300d5a"
 }
