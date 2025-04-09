55eada0cbcc7c52c65477ca6b7630b437a5a1944
Ankit Kanojia <119564533+ankitkanojia786@users.noreply.github.com>
2025-04-10 04:59:03 +0530

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -13,3 +13,9 @@ variable "github_branch" {
   default     = "main"
   description = "GitHub branch to monitor"
 }
+
+variable "codestar_connection_arn" {
+  description = "ARN of the AWS CodeStar GitHub connection"
+  type        = string
+  default     = "arn:aws:codestar-connections:ap-south-1:860265990835:connection/b9b175eb-c417-44dc-8e1b-332d71300d5a"
+}
