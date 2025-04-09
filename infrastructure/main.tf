64f247dec62f4cebe4f38c2f7db69463d981172d
Ankit Kanojia <119564533+ankitkanojia786@users.noreply.github.com>
2025-04-10 05:03:17 +0530

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,18 +1,11 @@
-# infrastructure/main.tf
-terraform {
-  required_version = ">= 1.5.0"
-}
-
-# Keep your existing provider.tf and backend.tf AS IS
-# Keep your existing outputs.tf AS IS
-
 module "react_app" {
   source = "./modules/react_app"
 
-  # Required variables
+  # Required arguments
   app_name                = "my-react-app"
   github_repo             = "ankitkanojia786/Deploying-a-React-Application-with-CI-CD"
   github_branch           = "main"
   s3_bucket_name          = "my-react-app-b38bc729"
   cloudfront_distribution_id = "E2G08P571G5PON"
+  codestar_connection_arn = "arn:aws:codestar-connections:ap-south-1:860265990835:connection/b9b175eb-c417-44dc-8e1b-332d71300d5a" # ADD THIS LINE
 }
