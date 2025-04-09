54d9d716fbb579c66ed718c1c3b79566b7d8e6c9
Ankit Kanojia <119564533+ankitkanojia786@users.noreply.github.com>
2025-04-10 03:56:58 +0530

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,7 +2,6 @@ terraform {
   required_version = ">= 1.5.0"
 }
 
-
 resource "aws_codestarconnections_connection" "github" {
   name          = "${var.app_name}-github-connection"
   provider_type = "GitHub"
@@ -14,6 +13,12 @@ resource "aws_codestarconnections_connection" "github" {
 module "react_app" {
   source = "./modules/react_app"
 
+  # Required arguments from earlier errors
+  s3_bucket_name            = "my-react-app-b38bc729"  # Your existing bucket
+  cloudfront_distribution_id = "E2G08P571G5PON"        # Your existing CloudFront ID
+  region                    = "ap-south-1"             # Mumbai region
+
+  # GitHub/Codestar arguments
   app_name                = var.app_name
   github_repo             = var.github_repo
   github_branch           = var.github_branch
