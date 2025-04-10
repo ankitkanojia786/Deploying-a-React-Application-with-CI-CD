9ef6f91b1ffac9202b7c07e5f5022a6b1e007103
Ankit Kanojia <119564533+ankitkanojia786@users.noreply.github.com>
2025-04-11 04:42:11 +0530

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -14,4 +14,8 @@ module "react_app" {
   s3_bucket_name          = "my-react-app-b38bc729"
   cloudfront_distribution_id = "E2G08P571G5PON"
   codestar_connection_arn = "arn:aws:codestar-connections:ap-south-1:860265990835:connection/b9b175eb-c417-44dc-8e1b-332d71300d5a"
+  
+  # New variables for email alerts
+  notification_email      = "ankitkanojia58@gmail.com.com" # << Replace with your email
+  region                  = "ap-south-1"            # << Match your current region
 }
