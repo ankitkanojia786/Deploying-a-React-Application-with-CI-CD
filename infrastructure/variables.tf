340d9a9cd89d22643808eecb0393bfe969923bf5
Ankit Kanojia <119564533+ankitkanojia786@users.noreply.github.com>
2025-04-11 04:43:57 +0530

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -19,3 +19,26 @@ variable "codestar_connection_arn" {
   type        = string
   default     = "arn:aws:codestar-connections:ap-south-1:860265990835:connection/b9b175eb-c417-44dc-8e1b-332d71300d5a"
 }
+
+# New variables for email alerts
+variable "notification_email" {
+  description = "Email address to receive pipeline failure notifications"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region where resources will be deployed"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "s3_bucket_name" {
+  description = "Name of the S3 bucket for hosting the React app"
+  type        = string
+}
+
+variable "cloudfront_distribution_id" {
+  description = "Existing CloudFront distribution ID (if any)"
+  type        = string
+  default     = ""
+}
